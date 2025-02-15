0679d982b7cc6ebf27aa08ad688445b42ab2ef6e
Justine Isaiah Salazar <justisaiahsalazar@gmail.com>
2025-02-15 22:58:57 +0800

    Add files via upload

---
+++ GEN Z DICTIONARY.py
@@ -0,0 +1,54 @@
+# GEN Z DICTIONARY
+DATA = {
+    "AURA":"This is a measuremen of how cool (positive aura) or embarrasing (negative aura) someone’s action is. (EX: Bro earned positive aura with that dance performance.)",
+    "BASIC":"A word to describe someone who likes mainstream things and is considered unoriginal. (EX: His taste in music is pretty basic.)",
+    "BET":"A term for agreement or approval. (EX: A: You still down to game tonight? ; B: Bet.)",
+    "BOP":"A really good song or beat. (EX: Yoasobi’s dicography is such a bop)",
+    "BOYCOTTING":"To stop  supporting  something or someone. (EX: I suggest we organize an immediate boycott of all Starbucks products.)",
+    "DOPE":"Very good, excellent, or impressive.(EX: That beat is so dope!)",
+    "DRIP":"A cool sense of style. It can refer to clothes or the way someone carries themselves. (EX: Look at these new shoes! My drip would be fire with them.)",
+    "GHOSTING":"To stop communicating with someone out of the blue. (EX: I can’t believe he is ghosting me! In the first place, he was the one who reached out!)",
+    "GIVING":"Used to describe someone or something emitting good or baad vibe. (Don’t you think he’s giving sus vibes?)",
+    "GLOW UP":"To go through a positive physical, mental, or spiritual change. (EX: He had a major glow up over the summer.)",
+    "GLAZING":"To overpraise something or someone. (EX: Why are people glazing on the new movie? It ain’t even that good.)",
+    "GOAT":"Short for “The Greatest Of All Time”. (EX: Lebron James is my one and only G.O.A.T.)",
+    "GREEN FLAG":"Used to decribe someone who’s actions and personality to be kind and positive. (EX: He’s such a green flag just how he took care of me earlier!)",
+    "LIT":"Exciting, fun, or excellent. (EX: The concert was so lit!)",
+    "LOW-KEY":"Subtle, not very obvious, or understated. (EX: I low-key hate this party but will pretend to enjoy it until it’s time to go home.)",
+    "NO CAP":"Saying “no cap” means that you aren’t lying, or if you say someone is “capping,” then you are saying they are lying. (EX: No cap, I saw them entering a hotel last night.)",
+    "NPC":"An acronym of non-playable character, a term used in gaming. It is used to describe someone with no individuality, originality, or personality. (EX: Jun is such an NPC.)",
+    "RED FLAG":"Used to describe someone who’s actions and personality to be bad and toxic. (EX: Stop seeing him, He’s such a red flag.)",
+    "RIZZ":"-Denotes someone's charisma or charm. (EX: I have unlimited rizz.)",
+    "SALTY":"describes a feeling of annoyance, bitterness, or resentment towards something or someone. (EX: Why are you being salty? He means nothing to me.)",
+    "SIGMA":"Refers to someone who is cool, independent, and successful, but who plays by their own rules. (EX: Denzel is such a sigma boy.)",
+    "SHEESH":"An exclamatory term used to express surprise, disbelief, or amazement. (EX: Sheesh, the weather is amazing today!)",
+    "SIMP":"Someone who shows excessive sympathy and attention toward another person, typically to someone who does not reciprocate the same feelings. (EX: Stop being a simp. She doesn’t like you back.)",
+    "SITUATIONSHIP":"A roamntic relationship that is not considered to be formal or established. (EX: We’ve been going out for 5 months, but we’re still in a situationship.)",
+    "SLAPS":"An adjective that describes how great something is. (EX: This burger slaps hard.)",
+    "SLAY":"To perform exceptionally well. (EX: You absolutely slayed the performance Queen! Good job.)",
+    "SMOL":"Used when a person finds something tiny and adorable at the same time. (EX: Your dog is so smol! I love it!)",
+    "STAN":"To be a fan of something or someone. (EX: We stan Lebron James)",
+    "SUS":"Short for suspicious and typically means shady or questionable. (EX: I’m not sure I want to go. One of your newfound friends is sus.)",  
+    "TEA":"refers to gossip, news, or information. (EX: Do you wanna hear the latest tea?)"
+    }
+a = input("What Word would you like to look up?:")
+print(DATA[a])
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
